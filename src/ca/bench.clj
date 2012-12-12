1c6356ea55e09a4c8f7e973c695d29f8fcf0450d
James Sofra <james.sofra@gmail.com>
2012-12-13 10:06:12 +1100

    add bench marking

--- src/ca/bench.clj
+++ src/ca/bench.clj
@@ -1,2 +1,16 @@
 (ns ca.bench
-  (:use criterium.core))
+  (:require [ca.matrix :as m])
+  (:use criterium.core ca.core))
+
+(defn bench-gol [w h]
+  (let [remaining-boards (atom (board-seq (m/set-borders! (m/rand-board w h) 0)))
+        next-board! (next-board-fn remaining-boards
+                                   (m/window-fn game-of-life-rule))]
+    (bench (next-board!))))
+
+(defn par-bench-gol [w h splits]
+  (let [remaining-boards (atom (board-view-seq
+                                (m/set-borders! (m/rand-board w h) 0) splits))
+        next-board! (par-next-board-fn remaining-boards
+                                   (m/window-fn game-of-life-rule))]
+    (bench (next-board!))))
