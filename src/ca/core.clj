1982e10024c84f7970c75a2636fff7b6b6e44b46
James Sofra <james.sofra@gmail.com>
2012-12-12 23:38:46 +1100

    add spilts keyword arg

--- src/ca/core.clj
+++ src/ca/core.clj
@@ -17,10 +17,11 @@
                      (m/set-pixels! init-image (m/apply-window-fn! b r rule-fn)))]
     (gui/start next-img! (.columns board) (.rows board))))
 
-(defn par-start-gui [board rule-fn]
+(defn par-start-gui [board rule-fn & {:keys [splits] :or {splits 2}}]
   (let [rule-fn (m/window-fn board rule-fn)
         remaining-boards (atom (board-seq (m/set-borders! board 0)))
         init-image (m/mat-to-image (-> @remaining-boards first first))
         next-img! #(let [[b r] (first (swap! remaining-boards rest))]
-                     (m/set-pixels! init-image (m/par-apply-window-fn! b r rule-fn 2)))]
+                     (m/set-pixels! init-image
+                                    (m/par-apply-window-fn! b r rule-fn splits)))]
     (gui/start next-img! (.columns board) (.rows board))))
